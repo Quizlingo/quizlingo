cb7be628b030e8299da952de908a207507766970
Brandon <bluong03@calpoly.edu>
2020-02-04 16:19:31 -0800

    add and read data

--- app/src/main/java/com/quizlingo/verticalprototype/MainActivity.kt
+++ app/src/main/java/com/quizlingo/verticalprototype/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.quizlingo.verticalprototype
 
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+        val TAG = "Test"
+
+        // Create a new user with a first and last name
+        val user1 = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user1)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+        // Create a new user with a first, middle, and last name
+        val user2 = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+
+        // Add a new document with a generated ID
+        db.collection("users")
+            .add(user2)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 }
