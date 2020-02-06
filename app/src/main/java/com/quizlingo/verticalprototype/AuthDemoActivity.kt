e426fc748ceddbeccb9a0e3af08e763a705bcf71
nguyen-darianhuy <17971850+nguyen-darianhuy@users.noreply.github.com>
2020-02-06 13:20:59 -0800

    AuthDemo: Add blank Activity

---
+++ app/src/main/java/com/quizlingo/verticalprototype/AuthDemoActivity.kt
@@ -0,0 +1,12 @@
+package com.quizlingo.verticalprototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthDemoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth_demo)
+    }
+}
