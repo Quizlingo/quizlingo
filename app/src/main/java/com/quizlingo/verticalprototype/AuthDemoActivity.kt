cc3c439a34da24dc5d2c481415adffd78c9b31e2
nguyen-darianhuy <17971850+nguyen-darianhuy@users.noreply.github.com>
2020-02-09 16:48:48 -0800

    Demo: Implement basic Login/Logout flow Activity

--- app/src/main/java/com/quizlingo/verticalprototype/AuthDemoActivity.kt
+++ app/src/main/java/com/quizlingo/verticalprototype/AuthDemoActivity.kt
@@ -1,12 +1,72 @@
 package com.quizlingo.verticalprototype
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import java.util.*
 
 class AuthDemoActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private val RC_SIGN_IN = 123;
+    private lateinit var button: Button
+    private lateinit var text: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth_demo)
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        button = findViewById<Button>(R.id.signin)
+        text = findViewById<TextView>(R.id.status)
+        button.setOnClickListener { launchSignInFlow() }
+        findViewById<Button>(R.id.signout).apply {
+            setOnClickListener { auth.signOut() }
+        }
+    }
+
+    private fun launchSignInFlow() {
+        // Give users the option to sign in / register with their email or Google account.
+        // If users choose to register with their email,
+        // they will need to create a password as well.
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        // Create and launch the sign-in intent.
+        // We listen to the response of this activity with the
+        // SIGN_IN_REQUEST_CODE.
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                text.text = "Signed in" + FirebaseAuth.getInstance().currentUser?.displayName
+            } else {
+                Log.e("Firebase", "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        auth.signOut()
     }
 }
